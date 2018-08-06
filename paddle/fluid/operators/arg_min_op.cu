3720e18196ba20dd25d91fa09a995c735a711a3e
sabreshao <sabre.shao@amd.com>
2018-08-06 10:25:39 +0800

    Remove eigen unsupported int16 and int8.

--- paddle/fluid/operators/arg_min_op.cu
+++ paddle/fluid/operators/arg_min_op.cu
@@ -24,8 +24,4 @@ REGISTER_OP_CUDA_KERNEL(
     paddle::operators::ArgMinKernel<paddle::platform::CUDADeviceContext,
                                     int32_t>,
     paddle::operators::ArgMinKernel<paddle::platform::CUDADeviceContext,
-                                    int16_t>,
-    paddle::operators::ArgMinKernel<paddle::platform::CUDADeviceContext,
-                                    size_t>,
-    paddle::operators::ArgMinKernel<paddle::platform::CUDADeviceContext,
-                                    uint8_t>);
+                                    size_t>);
