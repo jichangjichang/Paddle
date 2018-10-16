243eb52f6749821a2bfa1cef056df9d2eb507cb5
carlushuang <carlus.huang@amd.com>
2018-10-16 11:57:26 -0400

    add platform related code for amdgpu
    squash:
    74f724d  Add HIP support for fluid/memory
    52d3dc2  Add missing HIP staff in platform
    c3b631f  Add HIP support for fluid/platform and fluid/pybind
    
    bc5e7eb  fix build error for platform
    
    Signed-off-by: carlushuang <carlus.huang@amd.com>
    
    fix memory allocator porting error
    
    fix build error

--- paddle/fluid/platform/stream_callback_manager.h
+++ paddle/fluid/platform/stream_callback_manager.h
@@ -15,8 +15,12 @@
 #pragma once
 
 #include <ThreadPool.h>
+#ifdef PADDLE_WITH_CUDA
 #include <cuda.h>
 #include <cuda_runtime.h>
+#elif PADDLE_WITH_HIP
+#include <hip/hip_runtime_api.h>
+#endif
 #include <functional>
 #include <memory>
 #include "paddle/fluid/platform/enforce.h"
@@ -36,6 +40,7 @@ struct StreamCallbackContext {
   std::function<void()> callback_;
 };
 
+#ifdef PADDLE_WITH_CUDA
 class StreamCallbackManager {
  public:
   explicit inline StreamCallbackManager(cudaStream_t stream = nullptr)
@@ -80,6 +85,53 @@ class StreamCallbackManager {
     });
   }
 };
+#endif
+
+#ifdef PADDLE_WITH_HIP
+class StreamCallbackManager {
+ public:
+  explicit inline StreamCallbackManager(hipStream_t stream = nullptr)
+      : stream_(stream), thread_pool_(new ThreadPool(1)) {}
+
+  template <typename Callback>
+  inline void AddCallback(Callback &&callback) const {
+    auto *stream_callback_context =
+        new StreamCallbackContext(this, std::forward<Callback>(callback));
+    PADDLE_ENFORCE(hipStreamAddCallback(
+        stream_, StreamCallbackManager::StreamCallbackFunc,
+        stream_callback_context, 0));
+  }
+
+  template <typename Callback>
+  inline void AddCallbackWithStreamAndErrorInfo(Callback &&callback) const {
+    auto *stream_callback_context = new StreamCallbackContext(this, callback);
+    PADDLE_ENFORCE(hipStreamAddCallback(
+        stream_, StreamCallbackManager::StreamCallbackFunc,
+        stream_callback_context, 0));
+  }
+
+  void Wait() const { thread_pool_.reset(new ThreadPool(1)); }
+
+ private:
+  const hipStream_t stream_;
+  mutable std::unique_ptr<ThreadPool> thread_pool_;
+
+  // hipStreamCallback cannot call HIP API inside?
+  // so we have to use thread_pool here
+  static void StreamCallbackFunc(hipStream_t stream,
+                                           hipError_t status,
+                                           void *user_data) {
+    auto *callback_context_ptr =
+        reinterpret_cast<StreamCallbackContext *>(user_data);
+    callback_context_ptr->manager_->thread_pool_->enqueue([=]() {
+      std::unique_ptr<StreamCallbackContext> callback_context(
+          callback_context_ptr);
+      callback_context->callback_();
+    });
+  }
+};
+#endif
+
 
 }  // namespace platform
 }  // namespace paddle
