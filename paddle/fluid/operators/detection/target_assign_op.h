2883c4a1cc1cf69c2bc3c87d1766e169b0b98942
anghostcici <chenchennie1990@gmail.com>
2019-02-12 09:50:57 -0500

    add HIP support for target_assign_op

--- paddle/fluid/operators/detection/target_assign_op.h
+++ paddle/fluid/operators/detection/target_assign_op.h
@@ -106,7 +106,7 @@ class TargetAssignKernel : public framework::OpKernel<T> {
     int64_t k = x->dims()[2];
 
     auto x_lod = x->lod().back();
-#if defined(PADDLE_WITH_CUDA)
+#if (defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP))
     size_t* x_lod_data = x_lod.MutableData(ctx.GetPlace());
 #else
     size_t* x_lod_data = x_lod.data();
@@ -125,7 +125,7 @@ class TargetAssignKernel : public framework::OpKernel<T> {
       PADDLE_ENFORCE_EQ(neg_indices->lod().size(), 1UL);
       const int* neg_idx_data = neg_indices->data<int>();
       auto neg_lod = neg_indices->lod().back();
-#if defined(PADDLE_WITH_CUDA)
+#if (defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP))
       size_t* neg_lod_data = neg_lod.MutableData(ctx.GetPlace());
 #else
       size_t* neg_lod_data = neg_lod.data();
